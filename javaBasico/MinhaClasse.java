5627c8ea32b6f17fae94d1a78e2f89d97622905c
wagnernazarios <wagnernazario124@gmail.com>
2025-02-02 08:56:12 -0300

    introdução a anatomia de classes em Java

---
+++ javaBasico/MinhaClasse.java
@@ -0,0 +1,13 @@
+public class MinhaClasse {
+    // O nome da classe deve ser o mesmo do nome do arquivo
+    
+    public static void main(String[] args) {
+        System.out.print("Hello mundo!");
+
+        final String BR = "Brasil";
+        // ! */ BR = "Brasilia"; // Erro: não se pode alterar o valor de uma variável
+        
+
+        System.out.print(BR);
+    };
+}
